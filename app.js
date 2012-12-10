003c44b79cb1089e6c0df11189fe86f0315d8f68
Tiago Rodrigues <tmcrodrigues@gmail.com>
2012-12-10 01:09:04 +0000

    move configurations to config folder and create defaults

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var flatiron = require('flatiron'),
     feeds = require('./feeds');
 
 app.use(flatiron.plugins.http);
+app.config.use('file', {file: __dirname__+'/config/config.json'});
 
 app.router.get('/', function (data) {
   this.res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -15,7 +16,7 @@ app.router.post('/filter/repeatedkeywords', repeatedKeywordsController);
 app.router.post('/filter/scraper', scrapersController);
 
 feeds.createFetchers({
-  feedGroups: require('./feedlist2.json'),
+  feedGroups: require(app.config.get('feedlist')),
   createRoute: function(routePath, requestHandler) {
     app.router.get(routePath, requestHandler);
   },
